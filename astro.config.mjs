32d4a54b785b61b3533451aa6c6f33644b66ec03
jwwlvr <austria.lexie27@gmail.com>
2026-07-07 23:04:09 +0800

    fix: resolve duplicate keys in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-import icon from 'astro-icon';
+import icon from 'astro-icon'; 
 
 export default defineConfig({
-  integrations: [mdx(), react(), icon()],
-  site: 'https://jrgo7.github.io',
+  site: "https://beepatricio.github.io",
   base: 'virtual-exhibit-template',
+  integrations: [mdx(), react(), icon()],
 });
-
